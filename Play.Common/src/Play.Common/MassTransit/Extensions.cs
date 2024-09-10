2000fc88b69ae9dcf625143fac289a32685e621f
Trung Phan <ductrung.phanhuynh@gmail.com>
2024-09-10 09:14:11 -0500

    fix gitignore

---
+++ Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using MassTransit;
+using MassTransit.Definition;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Play.Common.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Play.Common.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+                
+                configure.UsingRabbitMq((context, configurator) =>
+                {
+                    var configuration = context.GetService<IConfiguration>();
+                    var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                    configurator.Host(rabbitMQSettings.Host);
+                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                });
+            });
+
+            services.AddMassTransitHostedService();
+            return services;
+        }
+    }
+}
