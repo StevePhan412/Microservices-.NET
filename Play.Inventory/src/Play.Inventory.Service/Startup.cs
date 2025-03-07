1b2baee689e775dc428898122a0b587c1bcb4425
aczio <43378561+StevePhan412@users.noreply.github.com>
2025-03-07 12:32:22 -0600

    VSCode json

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Net.Http;
-using Amazon.Runtime.Internal.Util;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
